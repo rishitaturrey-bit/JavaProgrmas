9f386d7c9f9162a4746b961069ee555704fc2102
Rishita Turrey <rishitaturrey@gmail.com>
2026-03-31 12:31:38 +0530

    carrental.java

---
+++ Unit 1 and 2/Carrental.java
@@ -0,0 +1,27 @@
+class CARRENTAL {
+    int CarId;
+    String CarType;
+    float Rent;
+
+    void GetCar(int id, String type) {
+        CarId = id;
+        CarType = type;
+    }
+
+    float GetRent() {
+        if (CarType.equals("Small Car")) Rent = 1000;
+        else if (CarType.equals("Van")) Rent = 800;
+        else if (CarType.equals("SUV")) Rent = 2500;
+        return Rent;
+    }
+
+    void ShowCar() {
+        System.out.println(CarId + " " + CarType + " " + GetRent());
+    }
+
+    public static void main(String[] args) {
+        CARRENTAL c = new CARRENTAL();
+        c.GetCar(1, "SUV");
+        c.ShowCar();
+    }
+}
