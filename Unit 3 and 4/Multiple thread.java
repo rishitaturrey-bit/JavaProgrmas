051518ed5aaa7839aee544789092d8180bfe79eb
Rishita Turrey <rishitaturrey@gmail.com>
2026-03-31 19:29:52 +0530

    program

---
+++ Unit 3 and 4/Multiple thread.java
@@ -0,0 +1,25 @@
+class Thread1 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread 1: " + i);
+        }
+    }
+}
+
+class Thread2 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread 2: " + i);
+        }
+    }
+}
+
+class TestThreads {
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+
+        t1.start();
+        t2.start();
+    }
+}
