1626973d8cd2b07982ab02cba3d9394a6af22422
Rishita Turrey <rishitaturrey@gmail.com>
2026-03-31 16:44:05 +0530

    program

---
+++ Unit 3 and 4/FileExtenstion.java
@@ -0,0 +1,16 @@
+public import java.util.Scanner;
+
+class FileExtensionExtractor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String file = sc.nextLine();
+
+        int index = file.lastIndexOf('.');
+        String ext = file.substring(index + 1);
+
+        System.out.println("Extension: " + ext);
+    }
+} {
+    
+}
